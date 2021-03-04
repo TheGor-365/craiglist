2f5fa274708ff78895742d06af5773b0bcfb76c1
Gor <7495500@gmail.com>
2021-03-04 18:56:16 +0300

    Customize sign in sign up pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     controller.controller_name == 'public' && controller.action_name == 'home'
   end
 
+  def is_auth?
+    controller.controller_name == 'sessions' || controller.action_name == 'registrations'
+  end
+
 end
